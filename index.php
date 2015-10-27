bdc387569ebff526a5258df50be035bdb9eea4b5
Sheyx <sheyxtd@gmail.com>
2015-10-28 01:31:34 +0300

    TEst

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IMEC.PRO</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="Content-Language" content="ru">
+    <script type="text/javascript" src="/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="/js/main.js"></script>
+</head>
+<body>
+Тело страницы
+asdasd
+asdasd
+</body>
+
+</html>
